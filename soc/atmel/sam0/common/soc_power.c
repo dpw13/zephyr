e1d60fcd412c8cb026d74a82966633de10082300
Dane Wagner <dane.wagner@gmail.com>
2025-02-27 13:34:04 -0600

    Use on-board LED to show power state

--- soc/atmel/sam0/common/soc_power.c
+++ soc/atmel/sam0/common/soc_power.c
@@ -5,21 +5,27 @@
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/pm/pm.h>
 #include <soc.h>
 
 LOG_MODULE_REGISTER(power, CONFIG_PM_LOG_LEVEL);
 
+#define LED0_NODE DT_ALIAS(led0)
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
 void pm_state_set(enum pm_state state, uint8_t substate_id)
 {
 	if (state == PM_STATE_ACTIVE) {
+		gpio_pin_set_dt(&led, 1);
 		/* Clear deepsleep bit */
 		SCB->SCR &= ~SCB_SCR_SLEEPDEEP_Msk;
 		return;
 	}
 
 	LOG_DBG("state %d substate %d", state, substate_id);
+	gpio_pin_set_dt(&led, 0);
 
 	switch (state) {
 		case PM_STATE_RUNTIME_IDLE:
@@ -58,6 +64,7 @@ void pm_state_exit_post_ops(enum pm_state state, uint8_t substate_id)
 	ARG_UNUSED(state);
 	ARG_UNUSED(substate_id);
 
+	gpio_pin_set_dt(&led, 1);
 	/* Clear PRIMASK */
 	__enable_irq();
 }
